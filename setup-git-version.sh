9a5731bef81ecfe34c8f67fcb74deb147b1b666f
Ádám Liszkai <adam@liszkai.dev>
2023-09-08 06:26:45 +0200

    feat: add linting and formatting

--- setup-git-version.sh
+++ setup-git-version.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
+TAG="$(git describe --tags --exact-match 2>/dev/null)"
+
 GDSCRIPT=$(
 	cat <<-EOF
 		extends Node
 
-		const Build = ${GITHUB_RUN_NUMBER:-null}
-		const Branch = "$(git rev-parse --abbrev-ref HEAD)"
-		const Commit = "$(git rev-parse --short HEAD)"
-		const Tag = "$(git describe --tags --exact-match 2>/dev/null)"
+		const BUILD = ${GITHUB_RUN_NUMBER:-null}
+		const BRANCH = "$(git rev-parse --abbrev-ref HEAD)"
+		const COMMIT = "$(git rev-parse --short HEAD)"
+		const TAG = ${TAG:-null}
 	EOF
 )
 
