7d4dffeb8490cb646a7bcdcb1a0637098508c204
Ádám Liszkai <adam@liszkai.dev>
2023-09-13 03:47:09 +0200

    fix(Player): re-create player scene after rebase

--- Game/Interactibles/Tok/TokBall.gd
+++ Game/Interactibles/Tok/TokBall.gd
@@ -19,6 +19,9 @@ func _ready() -> void:
 
 
 func _on_collision(body: Node3D) -> void:
+	if body is PlayerController:
+		print("%s collided while %s" % [body, body.movement])
+
 	if body is PlayerController and body.movement == Player.Movement.SLIDING:
 		var direction = 1.0 if body.velocity.normalized().x > 0.0 else -1.0
 		apply_impulse(Vector3(LAUNCH_FORCE_HORIZONTAL * direction, LAUNCH_FORCE_VERTICAL, 0.0))
